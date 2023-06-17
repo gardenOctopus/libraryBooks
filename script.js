617ee9c5de0ab581c7ec3df8030e1ec0e7db9718
gardenOctopus <zebraspider@protonmail.com>
2023-06-17 05:07:30 -0400

    Created form

--- script.js
+++ script.js
@@ -5,26 +5,11 @@ const books = [
     {title: 'The Second Sex'}
 ]
 
-//Counts Order of When Books Were Added
-let number = 0;
-
 //Creates New Book Object
 function createObject (title) {
     this.title = title
     return `${title}`
 }
-
-// //Displays Books
-// function displayBooks() {
-//     const shelf = document.getElementById('book-display');
-// let book = [];
-// books.forEach(book => {
-//     for (let key in book) {
-//         book.push(`${book[key]}`);
-//     }
-// })
-// shelf.innerHTML = book;
-// }
 
 function table() {
     //Gets Table
@@ -48,17 +33,7 @@ function table() {
 
 //Attaches addBook Function to Nav Link
 const addBookBtn = document.getElementById('add-book').addEventListener('click', () => {
-    // Ask User for Book
-    const userBook = prompt('Please choose a book to add to the library.');
-    // Takes User Feedback and Creates New Book Object
-    let title = new createObject(userBook);
-    // Pushes New Book Object into the Books Array
-    books.push(title);
-    // Displays the New Book
-    table();
-    //For Book Number
-    //let increaseNumber = ++number;
-    //table(increaseNumber);
+
 });
 
 //Displays Existing Books
